0a57246405365d0cd4c5159fe2c0c8cf86f2f2e7
Jason Rudland <knowlecules@gmail.com>
2014-11-03 10:47:16 -0500

    print reviews

--- test/integration/test_quality_reviews.py
+++ test/integration/test_quality_reviews.py
@@ -98,7 +98,7 @@ class MyTestCase(unittest.TestCase):
     def are_recent_reviews_complete(self, provider, has_rating=True, has_author=True,has_date=True, has_excerpt=True):
 
         reviews = Review.select().where(Review.provider == provider).where(Review.created_at > few_hours_ago)
-        print json.dumps(reviews)
+        print reviews
         if has_rating:
             reviews = reviews.select().where(Review.rating > 0)
 
